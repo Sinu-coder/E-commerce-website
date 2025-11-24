eb743c910a2adc32cfcb883be211a61c8afdb271
Sinu-coder <pandarabinarayana721@gmail.com>
2025-11-24 16:00:16 +0530

    fix: correct BlogNews import path

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import BlogHome from '../section/blog/BlogHome'
-import BlogNews from '../section/blog/BLogNews'
+import BlogNews from '../section/blog/BlogNews'
 import FooterSection from '../components/FooterSection'
 
 function Blog() {
